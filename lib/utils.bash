aff21f27252b6ea414c0cc666a93aba41e4fe4c4
dylan <dylanrayboss@gmail.com>
2025-02-19 12:56:05 -0600

    fix: moves binary from mix dir

--- lib/utils.bash
+++ lib/utils.bash
@@ -51,7 +51,7 @@ install_version() {
 		mix escript.install hex protobuf "$version" --force
 
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp "~/.mix/escript/$TOOL_NAME" "$install_path"
 
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
